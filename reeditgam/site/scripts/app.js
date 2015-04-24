8931365737ec2b0cbab1121152c2985792c23c13
mjorgesantos <santosjorge882@gmail.com>
2015-04-24 01:57:17 -0500

    03.post con objetos

--- reeditgam/site/scripts/app.js
+++ reeditgam/site/scripts/app.js
@@ -5,6 +5,11 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola angular";
-		$scope.post=["post1","post2","post3","post4","post5","post6"];
-
+		$scope.post=["{Title: "post1",upvotes:5}",
+		"{title: "post2", upvotes: 15}",
+		"{title: "post3", upvotes:  7}",
+		"{title: "post4", upvotes: 9}",
+		"{title: "post5",  upvotes: 3}",
+		"{title: "post6", upvotes: 5}"];
+		
 }]);
